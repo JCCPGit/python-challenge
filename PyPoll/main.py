de59fd8a7b1b722ec2c796a402b0bd9240350225
JCCPGit <cortezperezjc@gmail.com>
2021-03-30 10:15:09 -0700

    PyPoll completed version

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,43 +1,63 @@
 ## PyPoll
 
+# import required modules
 import os
 import csv
+from collections import Counter
 
+# set required variables
 number_votes = 0
+total = 0
 
+# create required lists
 candidates = []
-votes_by_candidate = []
+votes = []
 
+# path to collec data from Resources folder
 csv_path = os.path.join("Resources", "election_data.csv")
 
-with open(csv_path) as datafile:
+# module for reading CSV files
+with open(csv_path, 'r') as datafile:
     csv_reader = csv.reader(datafile, delimiter=',')
     csv_header = next(csv_reader)
 
     for row in csv_reader:
+        
+# Calculation of the total number of votes cast
         number_votes += 1
-
+        
+# List of the candidates who received votes
         if row[2] not in candidates:
             candidates.append(row[2])
+        
+# Calculation of the total number of votes each candidate won       
+        votes.append(row[2])
 
-        votes_by_candidate.append(row[2])
+votes_by_candidate = Counter(votes)
 
-number_of_candidates = len(candidates)
-print(number_of_candidates)
+# Calculation of the percentage of votes each candidate won
+values = votes_by_candidate.values()
+values_list = list(values)
+total = sum(values)
+percentage_by_candidate = [round(value * 100 / total, 2) for value in values]
 
-for x in range(number_of_candidates):
+# Winner of the election
+max_percentage = max(percentage_by_candidate)
+index = percentage_by_candidate.index(max_percentage)
 
-[[x, votes_by_candidate(x)] for x in set(votes_by_candidate)]
-
-
-print(candidates)
-
-
-
+# print of analysis
 print("Election Results")
 print("----------------------------")
 print(f'Total Votes: {number_votes}')
 print("----------------------------")
+print(f"{candidates[0]}: {percentage_by_candidate[0]}00% ({values_list[0]}) ")
+print(f"{candidates[1]}: {percentage_by_candidate[1]}00% ({values_list[1]}) ")
+print(f"{candidates[2]}: {percentage_by_candidate[2]}00% ({values_list[2]}) ")
+print(f"{candidates[3]}: {percentage_by_candidate[3]}00% ({values_list[3]}) ")
+print("----------------------------")
+print(f"Winner: {candidates[index]}")
+print("----------------------------")
+
 
 
 
